1927cedfb771568dad148c01d60a0d46ff9970ee
GM4747 <govindmohan47@gmail.com>
2024-04-01 23:55:22 +0530

    logout

---
+++ frontEnd/logout.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth, signOut  } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWvXfpM1gAA5nBPFauETnCfnuItBvx5mo",
+  authDomain: "cyber-login-a72ce.firebaseapp.com",
+  projectId: "cyber-login-a72ce",
+  storageBucket: "cyber-login-a72ce.appspot.com",
+  messagingSenderId: "433768429478",
+  appId: "1:433768429478:web:9dedffe819082888e6c167"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const logout=document.getElementById('logoutButton');
+logout.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      window.location.href = "login.html"; // Redirect to the login page or wherever you want
+    })
+    .catch((error) => {
+      // An error happened.
+      console.error(error);
+    });
+});
