85d7acc07ed92e023e7e64b50df15f2f65a8a755
anandrraj02 <anandrrajmce@gmail.com>
2024-04-04 13:06:22 +0530

    sidenav goback

--- frontEnd/side.js
+++ frontEnd/side.js
@@ -1,3 +1,25 @@
 document.getElementById("profButton").addEventListener("click", function () {
-  document.querySelector(".side").classList.toggle("slide-down");
-});
+  var sideNav = document.querySelector(".side");
+  if (sideNav.classList.contains("slide")) {
+       sideNav.classList.remove("slide");
+       sideNav.classList.add("slide-back");
+  } else {
+       sideNav.classList.remove("slide-back");
+       sideNav.classList.add("slide");
+  }
+ });
+document.addEventListener("click", function(event) {
+  var isClickInside = document.querySelector(".side").contains(event.target);
+  var sideNav = document.querySelector(".side");
+  var profButton = document.getElementById("profButton");
+ 
+  // Check if the click is outside the side navigation and not on the profile button
+  if (!isClickInside && event.target !== profButton) {
+       // If the side navigation is visible, hide it
+       if (sideNav.classList.contains("slide")) {
+           sideNav.classList.remove("slide");
+           sideNav.classList.add("slide-back");
+       }
+  }
+ });
+ 
