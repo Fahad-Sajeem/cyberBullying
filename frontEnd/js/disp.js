ca4ec64cd62d50bf32ec5bd188c7aff6ae68b5f4
GM4747 <govindmohan47@gmail.com>
2024-04-11 12:35:07 +0530

    disp maati

--- frontEnd/js/disp.js
+++ frontEnd/js/disp.js
@@ -1,20 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    // Your firebase configuration data
-  };
+  apiKey: "AIzaSyBWvXfpM1gAA5nBPFauETnCfnuItBvx5mo",
+  authDomain: "cyber-login-a72ce.firebaseapp.com",
+  projectId: "cyber-login-a72ce",
+  storageBucket: "cyber-login-a72ce.appspot.com",
+  messagingSenderId: "433768429478",
+  appId: "1:433768429478:web:9dedffe819082888e6c167",
+};
+
   
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
-  function showUserProfile() {
+  function showUserProfile() 
     const user = firebase.auth().currentUser;
-    if (user !== null) {
-      // The user object has basic properties such as display name, email, etc.
-      const displayName = user.displayName;
-      const email = user.email;
-      const photoURL = user.photoURL;
-      const emailVerified = user.emailVerified;
-      console.log("Display Name -", displayName, ", Email -", email, ", EmailVerified - ", emailVerified, " ,Photo Url -", photoURL);
-    }
-  }
+    if (user) {
+      // User is signed in, update the sidebar with the user's name and email
+      document.querySelector('.user h2').textContent = user.displayName || 'userName';
+      document.querySelector('.user k').textContent = user.email || 'userEmail';
+     } else {
+      // No user is signed in
+      console.log('No user is signed in.');
+     }
+     
 
 function updateUserDetails() {
     const user = firebase.auth().currentUser;
