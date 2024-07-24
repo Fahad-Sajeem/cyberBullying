954a99fd196f97615a9c9f0846ab582bdcafbf70
Fahad Sajeem <69871146+Fahad-Sajeem@users.noreply.github.com>
2024-07-24 11:38:18 +0530

    Update main.py

--- backEnd/Python/main.py
+++ backEnd/Python/main.py
@@ -5,8 +5,7 @@ import pandas as pd
 import os
 
 def process_video_id(videoId):
-    API_key = "AIzaSyBGTpbJlpXs195pbV1gDa11xKzdE2Nm_J0"
-    # videoId = "riXpu1tHzl0"
+    API_key = "youtubeAPI"
     
     # Fetch comments and get them as a DataFrame
     comments_df = fetch_comments(videoId, API_key)
@@ -17,15 +16,6 @@ def process_video_id(videoId):
 
     # calling function to convert the predicted to pdf file
     pdf_created = create_pdf(predicted_df)
-
-    # saving the csv file to specified path
-    # save_dir = os.path.join(os.getcwd(),'backEnd','OutputFiles')    
-    # os.makedirs(save_dir, exist_ok=True)
-    # csv_file_path = os.path.join(save_dir, 'predicted_comment_out_14.csv')
-
-    # # Save the DataFrame with predictions to a new CSV file
-    # predicted_df.to_csv(csv_file_path, index=False)
-    # print(f"Predictions saved to {csv_file_path}")
 
     return pdf_created
 
